52a383fe15a4ee6f8713214db3738515e91d2f76
Notand105 <urzua.fernando.f@gmail.com>
2024-03-31 17:56:29 -0300

    deploy?

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), svelte()]
+  integrations: [mdx(), sitemap(), svelte()],
+    site: 'https://urzuaf.github.io',
+  base: 'dbd',
 });
